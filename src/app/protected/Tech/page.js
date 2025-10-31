d9bc2900a5e093b062ab75b955cdd6e4489ce1bf
Atobatelemarcus <marcatobz3@gmail.com>
2025-10-31 16:48:03 +0100

    Refactor FeedPage component for improved readability

--- src/app/protected/Tech/page.js
+++ src/app/protected/Tech/page.js
@@ -49,7 +49,7 @@ export default function FeedPage({ tech = "Tech" }) {
           <hr className="bg-purple-600" />
           <div className="flex justify-between items-center gap-2">
             <div className="p-3">
-              <h2 className="text-lg font-semibold">{post.title}</h2>
+              <h2 className="md:text-lg text-sm font-semibold">{post.title}</h2>
               <p className="text-sm text-gray-500">
                 {post.content.slice(0, 100)}...
               </p>
@@ -71,4 +71,5 @@ export default function FeedPage({ tech = "Tech" }) {
     </div>
   );
 }
+
 
